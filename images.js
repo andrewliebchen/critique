41a9a4332a4f04adc28bfe2358497b8a3ca4317f
andrewliebchen <andrewliebchen@gmail.com>
2015-04-11 22:48:37 -0700

    Comments

--- images.js
+++ images.js
@@ -1,7 +1,11 @@
 Images = new Mongo.Collection('images');
 Placemarks = new Mongo.Collection('placemarks');
+Comments = new Mongo.Collection('comments');
 
 if (Meteor.isClient) {
+  Session.setDefault('currentPlacemark', null);
+  Session.setDefault('currentUser', null);
+
   Template.dropboxChooser.events({
     'click #mtr_dropbox' : function(event){
       event.preventDefault();
@@ -61,15 +65,45 @@ if (Meteor.isClient) {
   Template.placemarks.helpers({
     placemark: function(){
       return Placemarks.find({parent: this._id});
+    },
+
+    active: function(){
+      return Session.equals('currentPlacemark', this._id);
+    }
+  });
+
+  Template.placemarks.events({
+    'click .mtr_placemark': function(){
+      Session.set('currentPlacemark', this._id);
+    }
+  });
+
+  Template.comments.helpers({
+    comment: function(){
+      return Comments.find({parent: Session.get('currentPlacemark')});
+    }
+  });
+
+  Template.comments.events({
+    'click .mtr_add-comment': function(event, template){
+      var username = template.find('.mtr_user');
+      var message = template.find('.mtr_message');
+
+      if(message.value !== '') {
+        Meteor.call('addComment', {
+          parent: Session.get('currentPlacemark'),
+          name: username.value,
+          message: message.value,
+          createdAt: moment()
+        });
+
+        message.value = '';
+      }
     }
   });
 }
 
 if (Meteor.isServer) {
-  // Meteor.startup(function(){
-  //   Images.remove({});
-  // });
-
   Meteor.methods({
     'createImage': function(image){
       Images.insert(image);
@@ -84,6 +118,15 @@ if (Meteor.isServer) {
         parent: args.parent,
         top: args.top,
         left: args.left
+      });
+    },
+
+    'addComment': function(args){
+      Comments.insert({
+        parent: args.parent,
+        name: args.name,
+        message: args.message,
+        createdAt: args.createdAt
       });
     }
   });
