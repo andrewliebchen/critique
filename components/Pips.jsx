f96368fbe29d549dc77e88a7b9984fc3669f048d
Andrew Liebchen <andrewliebchen@gmail.com>
2016-07-01 23:58:26 -0700

    Tokenize pips, detect pip click

--- components/Pips.jsx
+++ components/Pips.jsx
@@ -2,8 +2,17 @@ import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import { $ } from 'meteor/jquery';
+import randomToken from 'random-token';
+
 
 export default class Pips extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activePips: [],
+    };
+  }
+
   render() {
     const { pips } = this.props;
     return (
@@ -15,6 +24,7 @@ export default class Pips extends Component {
           <div
             className="pip"
             key={i}
+            onClick={this.handlePipClick}
             style={{
               left: `${pip.x}%`,
               top: `${pip.y}%`,
@@ -28,13 +38,24 @@ export default class Pips extends Component {
     const pips = $(ReactDOM.findDOMNode(this.refs.pips));
     const pipsWidth = pips.outerWidth();
     const pipsHeight = pips.outerHeight();
+    const pipId = randomToken(5);
 
     Meteor.call('addPip', {
       imageId: this.props.imageId,
+      id: pipId,
       x: (event.pageX - pips.offset().left) / pipsWidth * 100,
       y: (event.pageY - pips.offset().top) / pipsHeight * 100,
       created_at: Date.now(),
+    }, (err, success) => {
+      if (success) {
+        this.setState({activePips: this.state.activePips.concat(pipId)});
+      }
     });
+  }
+
+  handlePipClick(event) {
+    event.stopPropagation();
+    console.log('click');
   }
 };
 
--- server/images.js
+++ server/images.js
@@ -14,6 +14,7 @@ Meteor.methods({
     return Images.update(args.imageId, {
       $push: {
         pips: {
+          id: args.id,
           x: args.x,
           y: args.y,
           created_at: args.created_at,
