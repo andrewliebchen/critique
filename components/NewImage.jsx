925025da80766da1bcc976c18b8aae9cb7a2ef59
Andrew Liebchen <andrewliebchen@gmail.com>
2016-07-05 12:51:59 -0700

    Add Rebass, style up add form

--- components/NewImage.jsx
+++ components/NewImage.jsx
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 import ReactDOM from 'react-dom';
 import { Router } from 'react-router';
+import { Button, Input, Slider } from 'rebass';
 import { Images } from '../api/main';
 
 export default class NewImage extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      url: null,
+      title: null,
       lifespan: 0,
     };
   }
@@ -15,39 +18,46 @@ export default class NewImage extends Component {
     const { lifespan } = this.state;
     return (
       <form onSubmit={this.handleAddImage.bind(this)}>
-        <div className="form-group">
-          <label>Dropbox URL</label>
-          <input type="url" ref="imageUrl" placeholder="Image URL"/>
-        </div>
-        <div className="form-group">
-          <label>Title</label>
-          <input type="text" ref="imageTitle" placeholder="Title"/>
-        </div>
-        <div className="form-group">
-          <label>Lifespan</label>
-          <input
-            type="range"
-            min="0"
-            max="36"
-            onChange={this.handleLifespan.bind(this)}
-            defaultValue={lifespan}/>
-          <span>{lifespan === 0 ? 'Forever' : `${lifespan} hours`}</span>
-        </div>
-        <input type="submit" value="Create"/>
+        <Input
+          type="url"
+          label="Dropbox URL"
+          name="imageUrl"
+          placeholder="Image URL"
+          onChange={this.handleUrlChange.bind(this)}/>
+        <Input
+          type="text"
+          label="Title"
+          name="imageTitle"
+          placeholder="Awesome design"
+          onChange={this.handleTitleChange.bind(this)}/>
+        <Slider
+          label="Image lifespan"
+          name="lifespan"
+          min="0"
+          max="36"
+          onChange={this.handleLifespan.bind(this)}
+          defaultValue={lifespan}/>
+        <p>{lifespan > 0 ? `${lifespan} hours` : 'Forever'}</p>
+        <Button onClick={this.handleAddImage.bind(this)}>Create</Button>
       </form>
     );
   }
 
+  handleUrlChange(event) {
+    this.setState({url: event.target.value});
+  }
+
+  handleTitleChange(event) {
+    this.setState({title: event.target.value})
+  }
+
   handleLifespan(event) {
-    const lifespan = event.target.value;
-    this.setState({lifespan: lifespan});
+    this.setState({lifespan: event.target.value});
   }
 
   handleAddImage(event) {
     event.preventDefault();
-    const { lifespan } = this.state;
-    const url = ReactDOM.findDOMNode(this.refs.imageUrl).value.trim();
-    const title = ReactDOM.findDOMNode(this.refs.imageTitle).value.trim();
+    const { url, title, lifespan } = this.state;
     const created_at = Date.now();
     if (url) {
       Meteor.call('addImage', {
