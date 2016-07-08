498a7fd835e2282fae027f790be913a478b43d7d
Andrew Liebchen <andrewliebchen@gmail.com>
2016-07-07 21:12:55 -0700

    Image upload to S3 with drag and drop, yo!

--- components/NewImage.jsx
+++ components/NewImage.jsx
@@ -4,11 +4,22 @@ import ReactDOM from 'react-dom';
 import { Router } from 'react-router';
 import {
   Button,
+  ButtonOutline,
   Container,
+  Card,
+  CardImage,
   Input,
   Slider,
+  Panel,
+  PanelHeader,
+  Text,
+  Section,
+  Divider,
+  Heading,
+  PanelFooter,
 } from 'rebass';
 import moment from 'moment';
+import Dropzone from 'react-dropzone-es6';
 import { Images } from '../api/main';
 
 export default class NewImage extends Component {
@@ -25,59 +36,60 @@ export default class NewImage extends Component {
     const { url, title, lifespan } = this.state;
     return (
       <Container>
-        <form>
-          <Button onClick={this.handleDropbox.bind(this)}>Dropbox</Button>
-          <Input
-            type="url"
-            label="Image URL"
-            name="imageUrl"
-            value={url}
-            placeholder="Image URL"
-            onChange={this.handleUrlChange.bind(this)}/>
-          <Input
-            type="text"
-            label="Title"
-            name="imageTitle"
-            value={title}
-            placeholder="Awesome design"
-            onChange={this.handleTitleChange.bind(this)}/>
-          <Slider
-            label={lifespan > 0 ? `Expires in ${lifespan} hours at ${moment().add(lifespan).fromNow()} at ${moment().add(lifespan).format('h:mma [on] dddd MMM Do')}` : 'Never expires'}
-            name="lifespan"
-            min="0"
-            max="36"
-            onChange={this.handleLifespan.bind(this)}
-            defaultValue={lifespan}
-            fill/>
-          <Button onClick={this.handleAddImage.bind(this)}>Create</Button>
-        </form>
+        {!url ?
+          <Dropzone
+            multiple={false}
+            onDrop={this.handleFileUpload.bind(this)}>
+            <div>Try dropping some files here, or click to select files to upload.</div>
+          </Dropzone>
+        :
+          <Card width={256}>
+            <CardImage src={url} />
+            <Heading level={3}>{title}</Heading>
+            <Text>
+              <a onClick={this.handleRemoveImage.bind(this)}>Remove</a>
+            </Text>
+          </Card>}
+        <Slider
+          label={lifespan > 0 ? `Expires in ${lifespan} hours at ${moment().add(lifespan).fromNow()} at ${moment().add(lifespan).format('h:mma [on] dddd MMM Do')}` : 'Never expires'}
+          name="lifespan"
+          min="0"
+          max="36"
+          onChange={this.handleLifespan.bind(this)}
+          defaultValue={lifespan}
+          mt={3}
+          fill/>
+        <div className={this.state.url ? '' : 'disabled'}>
+          <Button
+            onClick={this.handleAddImage.bind(this)}
+            mt={3}
+            style={{width: '100%'}}
+            big>
+            Create image
+          </Button>
+        </div>
       </Container>
     );
   }
 
-  handleDropbox(event) {
-    event.preventDefault();
-    Dropbox.choose({
-      linkType: 'direct',
-      multiselect: false,
-      extensions: ['.png', '.jpg', '.gif'],
-      success: (files) => {
-        files.forEach((file) => {
-          this.setState({
-            url: file.link,
-            title: file.name,
-          });
-        });
-      },
+  handleFileUpload(files) {
+    const file = files[0];
+    const uploader = new Slingshot.Upload('fileUploads');
+
+    uploader.send(file, (error, url) => {
+      if (error) {
+        console.error('Error uploading', uploader.xhr.response);
+      } else {
+        this.setState({
+          url: url,
+          title: file.name,
+        })
+      }
     });
   }
 
-  handleUrlChange(event) {
-    this.setState({url: event.target.value});
-  }
-
-  handleTitleChange(event) {
-    this.setState({title: event.target.value})
+  handleRemoveImage() {
+    this.setState({url: null});
   }
 
   handleLifespan(event) {
---
+++ server/fileUploads.js
@@ -0,0 +1,23 @@
+import { Meteor } from 'meteor/meteor';
+
+Slingshot.fileRestrictions('fileUploads', {
+  allowedFileTypes: ["image/png", "image/jpeg", "image/gif"],
+  maxSize: 10 * 1024 * 1024 // 10 MB (use null for unlimited)
+});
+
+Slingshot.createDirective('fileUploads', Slingshot.S3Storage, {
+  bucket: Meteor.settings.AWSBucketName,
+  acl: 'public-read',
+
+  authorize() {
+    // if (!this.userId) {
+    //   throw new Meteor.Error(403, "Login Required");
+    // }
+
+    return true;
+  },
+
+  key(file) {
+    return `${file.name}_${Date.now()}`;
+  },
+});
